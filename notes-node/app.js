b4039dfce4ca616bfe51f749bff76e6dec2820a2
github username <oliviatian27@gmail.com>
2019-01-11 16:48:49 -0500

    input

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,19 +1,22 @@
 console.log('starting app.js')
 
-const fs=require('fs')
-const os=require('os')
-const notes=require('./notes.js')
-
-const add=notes.add
-console.log(add(1,5))
-
-const _=require('lodash')
-
-console.log(_.isString('ttt'))
-console.log(_.uniq([1,2,1]))
+// const fs=require('fs')
+// const os=require('os')
+// const notes=require('./notes.js')
+//
+// const add=notes.add
+// console.log(add(1,5))
+//
+// const _=require('lodash')
+//
+// console.log(_.isString('ttt'))
+// console.log(_.uniq([1,2,1]))
 // let user=os.userInfo()
 //
 // fs.appendFile('greeting.txt',`hello ${user.username} !`,(err)=>{
 //   if (err) throw err
 //   console.log("appended")
 // })
+
+let command=process.argv[2]
+console.log('command: ',command)
