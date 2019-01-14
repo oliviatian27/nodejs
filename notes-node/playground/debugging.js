c3e5aa3d6aacacb231c756c5635031a7b1e44cd8
github username <oliviatian27@gmail.com>
2019-01-14 18:42:34 -0500

    debug

---
+++ notes-node/playground/debugging.js
@@ -0,0 +1,11 @@
+let person={
+  name:'some'
+}
+
+person.age=25
+
+debugger
+
+person.name='oaaaa'
+
+console.log(person)
---
+++ weather-app/playground/async-basics.js
@@ -0,0 +1,11 @@
+console.log('starting')
+
+setTimeout(()=>{
+  console.log('2000')
+},2000)
+
+setTimeout(()=>{
+  console.log('0')
+},0)
+
+console.log('finishing')
