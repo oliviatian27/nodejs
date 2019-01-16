b87e312bbacb5503fb2f9e6cf96a7fd5b0d36371
github username <oliviatian27@gmail.com>
2019-01-15 19:36:55 -0500

    hbs

--- web-server/server.js
+++ web-server/server.js
@@ -1,16 +1,32 @@
 const express=require('express')
+const hbs=require('hbs')
+
 const app=express()
+
+hbs.registerPartials(__dirname+'/views/partials')
+app.set('view engine',hbs)
+app.use(express.static(__dirname+'/public'))
+
+hbs.registerHelper('getCurrentYear',()=>{
+  return new Date().getFullYear()
+})
+
+hbs.registerHelper('screamIt',(text)=>{
+  return text.toUpperCase()
+})
 
 app.get('/', (req, res)=> {
   // res.send('Hello World')
-  res.send({
-    name:'aaa',
-    likes:[1,2,3]
+  res.render('home.hbs',{
+    pageTitle:'Home Page',
+    message:'welcome to node'
   })
 })
 
 app.get('/about',(req,res)=>{
-  res.send('about page')
+  res.render('about.hbs',{
+    pageTitle: 'About Page',
+  })
 })
 
 app.get('/bad',(req,res)=>{
@@ -19,4 +35,6 @@ app.get('/bad',(req,res)=>{
   })
 })
 
-app.listen(3000)
+app.listen(3000,()=>{
+  console.log('server is up on port 3000')
+})
