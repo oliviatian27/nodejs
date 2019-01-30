20542fb4e30c1bffc225dad93361afcf9a0e497a
github username <oliviatian27@gmail.com>
2019-01-30 17:07:28 -0500

    express

--- web-server/server.js
+++ web-server/server.js
@@ -1,17 +1,31 @@
 const express=require('express')
 const hbs=require('hbs')
+const fs=require('fs')
 
 const app=express()
 
 hbs.registerPartials(__dirname+'/views/partials')
 app.set('view engine',hbs)
-app.use(express.static(__dirname+'/public'))
+
 
 app.use((req,res,next)=>{
   let now= new Date().toString();
-  console.log(`${now}`)
+  let log=`${now}: ${req.method} ${req.url}`
+  console.log(log)
+  // console.log(res)
+  fs.appendFile('server.log',log+'\n',(err)=>{
+    if (err) {
+      console.log('unable to append to server.log')
+    }
+  })
   next()
 })
+
+// app.use((req,res,next)=>{
+//   res.render('maintenance.hbs')
+//   next()
+// })
+app.use(express.static(__dirname+'/public'))
 
 hbs.registerHelper('getCurrentYear',()=>{
   return new Date().getFullYear()
